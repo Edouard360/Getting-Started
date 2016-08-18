eaae8eafa438e1cd70bca07f3ab5410a1c641815
MEHLMAN <mailedouardmehlman@gmail.com>
2016-08-18 11:25:05 -0700

    Orange square data setup initial commit

--- webgl-demo.js
+++ webgl-demo.js
@@ -4,8 +4,6 @@ var squareVerticesBuffer;
 var shaderProgram;
 var vertexPositionAttribute;
 
-
-
 function start() {
   canvas = document.getElementById("glcanvas");
   gl = canvas.getContext("webgl");
@@ -13,21 +11,6 @@ function start() {
   gl.clearColor(27.0/255,27.0/255,27.0/255, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-  //************** Creating the buffers ************************//
-
-  squareVerticesBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-
-  var vertices = [
-    0.2,  0.2,  0.0,
-    -0.2, 0.2,  0.0,
-    0.2,  -0.2, 0.0,
-    -0.2, -0.2, 0.0
-  ];
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   //************** Creating the shaders ************************//
 
@@ -50,21 +33,33 @@ function start() {
 
   gl.useProgram(shaderProgram);
 
+  //************** Creating the buffers ************************//
+
+  squareVerticesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+  var vertices = [
+    0.2,  0.2,  0.0,
+    -0.2, 0.2,  0.0,
+    0.2,  -0.2, 0.0,
+    -0.2, -0.2, 0.0
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
   //************** Preparing to send data to the program ************************//
 
   vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "position");
   gl.enableVertexAttribArray(vertexPositionAttribute);
-
+  //************** Send data of the program ************************//
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
   drawScene();
 }
 
 function drawScene() {
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  
-  //Send data of the program
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   requestAnimationFrame(drawScene);
