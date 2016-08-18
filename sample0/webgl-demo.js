9e25240bdbf943d91d01580c485474bd9cc2cf22
MEHLMAN <mailedouardmehlman@gmail.com>
2016-08-18 11:12:58 -0700

    Black canvas original commit

--- sample0/webgl-demo.js
+++ sample0/webgl-demo.js
@@ -4,6 +4,8 @@ var squareVerticesBuffer;
 var shaderProgram;
 var vertexPositionAttribute;
 
+
+
 function start() {
   canvas = document.getElementById("glcanvas");
   gl = canvas.getContext("webgl");
@@ -11,7 +13,6 @@ function start() {
   gl.clearColor(27.0/255,27.0/255,27.0/255, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-
   drawScene();
 }
 
